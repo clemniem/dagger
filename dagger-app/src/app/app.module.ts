0af0251a14f2a2509a181ff81851bdc92cb89431
cni <clemens.niemeyer@smarter-ecommerce.com>
2018-06-29 16:43:10 +0200

    Add Components

--- dagger-app/src/app/app.module.ts
+++ dagger-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaggerComponent } from './tagger/tagger.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaggerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ dagger-app/src/app/tagger/tagger.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dagger-tagger',
+  templateUrl: './tagger.component.html',
+  styleUrls: ['./tagger.component.css']
+})
+export class TaggerComponent {
+
+  private swipeCoord?: [number, number];
+  private swipeTime?: number;
+
+  // https://stackoverflow.com/questions/42592156/what-is-the-best-way-to-implement-swipe-navigation-in-angular-2
+  swipe(e: TouchEvent, when: string): void {
+    const coord: [number, number] = [e.changedTouches[0].pageX, e.changedTouches[0].pageY];
+    const time = new Date().getTime();
+
+    if (when === 'start') {
+      this.swipeCoord = coord;
+      this.swipeTime = time;
+    }
+
+    else if (when === 'end') {
+      const direction = [coord[0] - this.swipeCoord[0], coord[1] - this.swipeCoord[1]];
+      const duration = time - this.swipeTime;
+      console.log(direction);
+
+      if ((duration < 1000 && (Math.abs(direction[0]) > 30 || Math.abs(direction[1]) > 30)) &&
+          true)
+          // Math.abs(direction[0]) > Math.abs(direction[1] * 3))
+      {
+        let swipe;
+        if (Math.abs(direction[0]) >= Math.abs(direction[1])) {
+          swipe = direction[0] > 0 ? 'right' : 'left';
+        } else {
+          swipe = direction[1] < 0 ? 'up' : 'down';
+        }
+        //Do whatever you want with swipe
+        console.log(swipe)
+      }
+    }
+  }
+
+}
