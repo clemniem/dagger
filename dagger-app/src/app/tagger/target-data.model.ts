a7bade934ba3f2a4c06559b4fffb043b7ba763b1
cni <clemens.niemeyer@smarter-ecommerce.com>
2018-08-03 14:40:24 +0200

    Remodel target-data.model

--- dagger-app/src/app/tagger/target-data.model.ts
+++ dagger-app/src/app/tagger/target-data.model.ts
@@ -2,14 +2,17 @@ export class TargetData {
   public id: string;
   public name: string;
   public imageUrl: string;
-  public tags: Map<string, string>;
+  public tag: string;
+  public left: string;
+  public right: string;
 
-
-  constructor(id: string, name: string, imageUrl: string, tags: Map<string, string>) {
+  constructor(id: string, name: string, imageUrl: string, tag: string, left: string, right: string) {
     this.id = id;
     this.name = name;
     this.imageUrl = imageUrl;
-    this.tags = tags;
+    this.tag = tag;
+    this.left = left;
+    this.right = right;
   }
 }
 
